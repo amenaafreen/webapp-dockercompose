a483913a406891862443a0c045486069912153db
aafreen <aafreen@USA-MAC-NIS1713.local>
2019-12-17 12:21:41 -0800

    dsl commit

--- web.groovy
+++ web.groovy
@@ -23,7 +23,7 @@ folder(basePath) {
      shell ("""docker-compose down &&\
                docker-compose up -d &&\
                echo "APP URL" &&\
-               curl http://localhost:8903/LoginWebApp/""")
+               echo "http://localhost:8903/LoginWebApp/" """)
        
      }
 }     
