23b80bcb0b6633df6b736ca026c212188a66c4a0
aafreen <aafreen@USA-MAC-NIS1713.local>
2019-12-17 10:40:41 -0800

    dsl script

---
+++ web.groovy
@@ -0,0 +1,40 @@
+String basePath = 'folder'
+folder(basePath) {
+    description('Folder containing all jobs for folder-1')
+}
+   job("$basePath/Job-scm-checkout") {
+   stages{
+   stage('Git checkout')
+   {
+   steps{
+    scm {
+        github('amenaafreen/demo-java', 'master')
+    }
+   triggers {
+     githubPush()
+    }
+}
+}
+   stage('Compile and Package')
+   {
+   steps {
+        shell('mvn clean package')
+        }
+   publishers {
+       //archive the war file generated
+       archiveArtifacts 'target/*.war'
+}
+}
+ stage('Deploy')
+{
+
+     steps{
+
+     shell ("""docker-compose up &&\
+           echo "APP URL" &&\
+           curl http://localhost:8903/LogicWebApp/""")
+       
+     }
+}     
+}     
+}
