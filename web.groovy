85bdeb65eb7e591ed81a5abf29fb2db2b299b120
aafreen <aafreen@USA-MAC-NIS1713.local>
2019-12-17 10:58:24 -0800

    dsl commit

--- web.groovy
+++ web.groovy
@@ -2,30 +2,21 @@ String basePath = 'folder'
 folder(basePath) {
     description('Folder containing all jobs for folder-1')
 }
-   job("$basePath/Job-scm-checkout") {
-   stage('Git checkout')
-   {
-   steps{
+   job("$basePath/scm-checkout") {
+  
     scm {
         github('amenaafreen/demo-java', 'master')
     }
    triggers {
      githubPush()
     }
-}
-}
-   stage('Compile and Package')
-   {
+
    steps {
         shell('mvn clean package')
         }
    publishers {
        //archive the war file generated
        archiveArtifacts 'target/*.war'
-}
-}
- stage('Deploy')
-{
 
      steps{
 
