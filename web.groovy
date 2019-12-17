b7b3f559323ed63b43f5ec3638b174e170806ea1
aafreen <aafreen@USA-MAC-NIS1713.local>
2019-12-17 11:41:28 -0800

    dsl commit

--- web.groovy
+++ web.groovy
@@ -21,7 +21,7 @@ folder(basePath) {
      steps{
 
      shell ("""docker-compose down &&\
-               docker-compose up -d &&\
+               docker-compose up &&\
            echo "APP URL" &&\
            curl http://localhost:8903/LogicWebApp/""")
        
