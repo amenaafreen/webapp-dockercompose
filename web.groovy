09bd2c6479e3b3eaf7da0fab8757518f26472bc1
aafreen <aafreen@usa-mac-nis1713.attlocal.net>
2019-12-18 20:52:43 -0800

    web.groovy modified

--- web.groovy
+++ web.groovy
@@ -25,19 +25,4 @@ folder(basePath) {
                curl -Is http://localhost:8903/LoginWebApp/""")
        
      }
-
-postBuildSteps {
-    maven {
-            property('groupId', 'com.spring.maventest')
-            property('artifactId', 'nexusartifact')
-            property('version', '1.0.0')
-            property('packaging', 'war')
-            property('repositoryId', 'nexus')
-            property('url', 'http://localhost:8051/#admin/repository/repositories:maven-repo')
-            property('file', 'target/LoginWebApp.war')
-           }
-
-
-}
-
 }
